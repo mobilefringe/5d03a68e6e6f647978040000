424a98a28c254ad558bb07f6ec729576dc491b6c
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-14 11:13:00 -0400

    Accessibility Compliance 2.0  /footer.vue

--- footer.vue
+++ footer.vue
@@ -41,7 +41,7 @@
                             <a :href="item.link" target="_blank">
                                 <div class="insta-feed-background" v-bind:style="{ backgroundImage: 'url(' + item.images.standard_resolution.url + ')' }" ></div>
                                 <!--<img :src="item.images.low_resolution.url" :alt="item.id"/>-->
-                                <span class="visibiltyhidden">Instagram feed</span>
+                                <span class="visibilityhidden">Instagram feed</span>
                             </a>
                         </div>
                     </div> 
